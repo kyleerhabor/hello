4b26725eed3748f7852ec98e2e62211e0fd36b8e
Kyle Erhabor <kyleerhabor@gmail.com>
2025-01-05 01:59:22 -0500

    Reimplement in Svelte

---
+++ src/routes/ExternalLink.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Link from "./Link.svelte";
+  import { isLocalURL } from "$lib/index";
+
+  const { href, url, children } = $props();
+  const isLocal = isLocalURL(href, url);
+</script>
+
+<Link {isLocal}>
+  <a {href} target={isLocal ? "_self" : "_blank"}>
+    {@render children()}
+  </a>
+</Link>
---
+++ src/routes/Link.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { ExternalLink } from "lucide-svelte";
+  import "../style/main.css";
+
+  const { isLocal, children } = $props();
+</script>
+
+<span class="link">
+  <span>
+    {@render children()}
+  </span>
+  <span class:hide={isLocal}>
+    <ExternalLink size="14px" />
+  </span>
+</span>
+
+<style>
+  .link {
+    display: inline flex;
+    gap: 2px;
+    align-items: baseline;
+  }
+</style>

020c255ea525c29036b6ede2edd331c0d35b5627
Kyle Erhabor <kyleerhabor@gmail.com>
2025-03-15 20:05:05 -0400

    Rewrite about in Markdown

--- src/routes/series/+page.server.js
+++ src/routes/series/+page.server.js
@@ -9,7 +9,7 @@ export async function load() {
   const mediums = key(idProp, data.mediums);
   const titles = key(idProp, data.titles);
   const logs = unique((log) => log.titleID, data.logs.toReversed());
-  const colors = await Promise.allSettled(R.map(async (log) => {
+  const colors = await Promise.allSettled(logs.map(async (log) => {
     const id = log.titleID;
     const root = dev ? "../../../static" : "../../../../client";
     const path = titles[id].coverImagePath;
@@ -18,7 +18,7 @@ export async function load() {
       titleID: id,
       accentColor: await getColor(new URL(`${root}/${path}`, import.meta.url).pathname),
     };
-  }, logs));
+  }));
 
   colors.forEach((result) => {
     if (result.status != "fulfilled") {

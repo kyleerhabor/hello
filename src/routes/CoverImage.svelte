d0d10a7ec714c93cf99d8ec4ac03614e0d2b2968
Kyle Erhabor <kyleerhabor@gmail.com>
2025-03-26 19:40:16 -0400

    Don't shift layout when rendering cover images

--- src/routes/CoverImage.svelte
+++ src/routes/CoverImage.svelte
@@ -1,24 +1,27 @@
 <script>
   const { assets, accentColor } = $props();
 
+  // Do we need $derived here?
   const color = accentColor ? `rgb(${accentColor[0]},${accentColor[1]},${accentColor[2]})` : undefined;
   const asset = assets[assets.length - 1];
 </script>
 
-<picture class="cover-image"
-         style:--accent-color={color}>
-  <!-- FIXME: Background color creates a small line below the image  -->
-  {#each assets.slice(0, -1) as asset}
-    <source srcset={asset.path} type={asset.mimeType}>
-  {/each}
-  {#if asset}
-    <img class="cover-image-item"
-         src={asset.path}
-         alt="Cover"
-         decoding="async"
-         loading="lazy">
-  {/if}
-</picture>
+<div class="cover-image"
+     style:--accent-color={color}>
+  <picture>
+    <!-- FIXME: Background color creates a small line below the image  -->
+    {#each assets.slice(0, -1) as asset}
+      <source srcset={asset.path} type={asset.mimeType}>
+    {/each}
+    {#if asset}
+      <img class="cover-image-item"
+           src={asset.path}
+           alt="Cover"
+           decoding="async"
+           loading="lazy">
+    {/if}
+  </picture>
+</div>
 
 <style>
   .cover-image {

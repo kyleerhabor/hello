e16fa0de582b0fd789b0fafa8c5fdc66cf783e35
Kyle Erhabor <kyleerhabor@gmail.com>
2025-03-30 19:13:25 -0400

    Set paths

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 set -e # Exit if any command fails
 
-export CPLUS_INCLUDE_PATH="$CWD/include:$CPLUS_INCLUDE_PATH"
-export LIBRARY_PATH="$CWD/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="$CWD/lib:$LD_LIBRARY_PATH"
-
 CWD="$(pwd)"
 SRCDIR=submodules
 BUILDDIR=preinstall

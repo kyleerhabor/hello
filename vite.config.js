f3520006ef25ff35c6ed51dfd132e7a56322898a
Kyle Erhabor <kyleerhabor@gmail.com>
2025-04-20 16:50:23 -0400

    Fix series recs. not loading

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
   plugins: [
-    sveltekit(),
+    sveltekit({
+      dynamicCompileOptions({ filename, compileOptions }) {
+        // https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md#dynamiccompileoptions
+        //
+        // Do they expect us to define forceRunesMode?
+        if (forceRunesMode(filename) && !compileOptions.runes) {
+          return { runes: true };
+        }
+      },
+    }),
   ],
 });

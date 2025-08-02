5b6e3395fcac66a05486e2ba39a4032e52bd1852
Kyle Erhabor <kyleerhabor@gmail.com>
2025-08-02 12:09:10 -0400

    Add Vite plugin for Chromium DevTools

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   plugins: [
     sveltekit({
       dynamicCompileOptions({ filename, compileOptions }) {
         // https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md#dynamiccompileoptions
-        //
-        // Do they expect us to define forceRunesMode?
         if (forceRunesMode(filename) && !compileOptions.runes) {
           return { runes: true };
         }
       },
     }),
+    devtoolsJson(),
   ],
 });

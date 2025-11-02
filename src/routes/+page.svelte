cfa9ca907e9385eb818521299c9235c0725d43fa
Kyle Erhabor <kyleerhabor@gmail.com>
2025-11-02 17:10:11 -0500

    Update about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,18 @@
 </svelte:head>
 
 <!-- TODO: Figure out how to use AsciiDoc -->
-<p>Hi!</p>
-
-<p>I'm Kyle Erhabor, a software developer studying Computer Science at Suffolk University in Boston, MA. I spend my days
-  studying and developing software.</p>
-
-<p>My current interests are in application development and systems programming for Apple's Darwin platform. Some notable
-  works of mine are the <TextLink href="https://github.com/kyleerhabor/advance">Advance</TextLink> image viewer app and
-  the <TextLink href="https://github.com/kyleerhabor/sampled">Sampled</TextLink> music player app.</p>
-
-<p>If you'd like to reach me, the best way is to send me an email. I enjoy reading them and will try to respond in a
-  timely manner.</p>
+<p>
+  Hi!
+</p>
+<p>
+  I'm Kyle Erhabor, a software developer studying Computer Science at Suffolk University in Boston, MA. I spend my days
+  studying and developing software.
+</p>
+<p>
+  My current interests are in application development and systems programming for Apple's Darwin platform. Some notable
+  works of mine are the <TextLink href="https://github.com/kyleerhabor/sampled">Sampled</TextLink> music player app and
+  <TextLink href="https://github.com/kyleerhabor/advance">Advance</TextLink> image viewer app.
+</p>
+<p>
+  If you'd like to reach me, the best way is to send me an email. I enjoy reading them and will try to respond soon.
+</p>
